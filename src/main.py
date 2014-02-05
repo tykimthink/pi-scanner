d43a95dbf85200492e5c9568d38b67ab0e31dcef
Ben Marshall <bm12656@my.bristol.ac.uk>
2014-02-05 13:26:34 +0000

    Updated main interface

---
+++ src/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This file is the main start point for the server program. It initialises the webserver, connects
+to the camera and performs general housekeeping.
+"""
+
+import server
+
+def main():
+	"""
+	The main entry point. This is the first function called when the program is started standalone.
+	"""
+	print("PI-Scanner Server\n------------------------------\n")
+
+	print("Initialising Interface...")
+	iserver = server.InterfaceServer(('0.0.0.0', 8080), server.APIHandler)
+	print("Server Started. Use the shutdown button to close it.")
+	iserver.serve_forever()
+
+
+if(__name__=="__main__"):
+	# script is the main module so call main()
+	main()
+else:
+	# the module has been imported so do nothing!
+	pass
