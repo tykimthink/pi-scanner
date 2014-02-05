3c12b94720f645e97cabc5a87d82a149dbe06644
Ben Marshall <bm12656@my.bristol.ac.uk>
2014-02-05 13:25:47 +0000

    first commit of the server module that will handle all API calls from the html client

---
+++ src/server.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Contains the server component class for the application. It handles all web interface features
+including client-side API calls.
+"""
+
+import http.server
+from socketserver import ThreadingMixIn
+import threading
+
+import camera
+
+apiPrefix = "/API/"
+apiArgStringSep = "?"
+apiArgStringDelim = "&"
+apiClassFuncSep = "."
+class APIHandler(http.server.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        if(self.path.startswith(apiPrefix)):
+            # All API request calls are handled and parsed within this function.
+            # They are then passed onto the appropriate handler function.
+            args = {}
+            function = self.path.replace(apiPrefix, "").split(apiArgStringSep)[0]
+            page = "self"
+            if(apiClassFuncSep in function):
+                page = function.split(apiClassFuncSep)[0]
+                function = function.split(apiClassFuncSep)[1]
+            if(apiArgStringSep in self.path):
+                argString = self.path.split("?")[1]
+                for kvp in argString.split("&"):
+                    key = kvp.split("=")[0]
+                    value = kvp.split("=")[1]
+                    args[key] = value
+                    print(key+":\t"+value)
+            print("Page: \t"+page)
+            print("Function:\t"+function)
+        else:
+            super().do_GET()
+
+class InterfaceServer(ThreadingMixIn, http.server.HTTPServer):
+    """Handle requests in a separate thread."""
+
+def test():
+    print("server.py testbench is running....")
+
+if(__name__=="__main__"):
+    # script is the main module so run the tests....
+    test()
+else:
+    # the module has been imported so do nothing!
+    pass
